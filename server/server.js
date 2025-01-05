6bc2531358466b39b777bad8777e543d94934bde
Ishini <ishinishavindhya@gmail.com>
2025-01-05 13:26:47 +0530

    dbconnection

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+cors({
+    origin : process.env.CLIENT_URL,
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+//middleware
+app.use(express.json());
+
+//db connection
+mongoose.connect(MONGO_URI)
+.then(() => console.log("mongodb is connected"))
+.catch((err) => console.log(err));
+
+app,use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).json({
+        success : false,
+        message : 'Something went wrong',
+    })
+})
+
+app.listen(PORT,()=>{
+    onsole.log("server is running on port ${PORT}");
+});
+
+
+
